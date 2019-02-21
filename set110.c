f256b6370de936d9f64cabcdc4a52a4e0e0bbe8f
abarna232 <46838608+abarna232@users.noreply.github.com>
2019-02-21 15:40:40 +0530

    set110.c

---
+++ set110.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void main()
+{
+    char a[10],b[10];
+    int k=0,l=0,i,j,c1=0,c2=0;
+    gets(a);
+    gets(b);
+    for(i=0;a[i]!='\0';i++)
+    {
+        c1++;
+    }
+    for(i=0;b[i]!='\0';i++)
+    {
+        c2++;
+    }
+     if(c1==c2)
+     {
+         for(i=0;i<c1;i++)
+         {
+             if(a[i]!=b[i])
+             {
+                 k++;
+             }
+             else
+             {
+                 l++;
+             }
+         }
+     }
+     else
+     {
+         printf("no");
+     }
+     if(c1-l==1)
+     {
+         printf("yes");
+     }
+     else
+     {
+         printf("no");
+     }
+}
