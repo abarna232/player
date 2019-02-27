03940c76f75488d92208f1bd86acdc8bb432c430
abarna232 <46838608+abarna232@users.noreply.github.com>
2019-02-27 16:08:45 +0530

    64.c

---
+++ 64.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+    int a[100],n,k,i,t,j;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<k)
+        {
+            printf("%d ",a[i]);
+        }
+    }
+}
