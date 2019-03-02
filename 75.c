c8cc9190606bd182be788a2de1b93a55c3e2de4e
abarna232 <46838608+abarna232@users.noreply.github.com>
2019-03-02 08:31:32 +0530

    75.c

---
+++ 75.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int n,a[100],i,j,c=0;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i;j<n;j++)
+{
+if(a[i]<a[j])
+{
+c++;
+}
+}
+}
+printf("%d",c);
+}
