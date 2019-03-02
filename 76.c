b42d573ea16cf17e22bf1f3b6b8195ce66fb5b99
abarna232 <46838608+abarna232@users.noreply.github.com>
2019-03-02 08:37:49 +0530

    76.c

---
+++ 76.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+int n,a[100],i,c=0,d=0,k,m;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+if(a[i]%2==0)
+{
+c++;
+k=a[i];
+}
+else 
+{
+d++;
+m=a[i];
+}
+}
+if(c==1)
+{
+printf("%d",k);
+}
+else 
+{
+printf("%d",m);
+}
+}
